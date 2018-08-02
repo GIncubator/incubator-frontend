7e5c1856ab08967187200de5c235ee75e52c543f
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2018-08-02 18:10:05 +0530

    Basic setup of the front end app and login system

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+        return (
+            <footer className="app-footer">
+                <div className="d-flex flex-row justify-content-between">
+                    <span> Copyright Company Name &copy; 2018</span>
+                </div>
+            </footer>
+        );
+    }
+;
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootEl = document.getElementById('app-site');
+
+// Create a reusable render method that we can call more than once
+let render = () => {
+    // Dynamically import our main App component, and render it
+    const MainApp = require('./MainApp').default;
+    ReactDOM.render(
+        <MainApp/>,
+        rootEl
+    );
+};
+
+if (module.hot) {
+    module.hot.accept('./MainApp', () => {
+        const NextApp = require('./MainApp').default;
+        render(
+            <NextApp/>,
+            rootEl
+        );
+    });
+}
+
+render();
