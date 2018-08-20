00fa5743020d2995b1addad71d7e1e15eb0abbde
Subrat <06.subrata@gmail.com>
2018-08-20 23:48:20 +0530

    prod firebase config added

--- config.js
+++ config.js
@@ -14,13 +14,16 @@ const devConfig = {
 
 const prodConfig = {
   FireBase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
-  }
+    apiKey: "AIzaSyCLL5ZFtg4Y3_ORIHtOepa22AHoGYukIw4",
+    authDomain: "gusec-incubator-project.firebaseapp.com",
+    databaseURL: "https://gusec-incubator-project.firebaseio.com",
+    projectId: "gusec-incubator-project",
+    storageBucket: "gusec-incubator-project.appspot.com",
+    messagingSenderId: "990443185412"
+  },
+  apiGateway: {
+    URL: 'http://localhost:4000'
+  },
 }
 
 const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
